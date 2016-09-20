3131174c5028b7e3e9726b7bfbeea4c11ae6d183
yangjingui <791348547@qq.com>
2016-09-20 20:45:04 +0800

    “过登录界面使用cookie记住登录密码功能实现“

--- src/js/register.js
+++ src/js/register.js
@@ -1,41 +1,52 @@
 jQuery(function($){
 	// 表单验证插件
-	var $form = $('.login_info').find('form');
-	$('#regForm').validate({
-		rules: {
-            email:{
-                required: true,
-                email: true
-            },
-            psw:{
-                required: true,
-                minlength: 6,
-                maxlength: 16
-            },
-            repsw:{
-                required: true,
-                equalTo: "#psw"
-            },
-            check: {
-                required: true               
-            }
-        },
-        messages:{
-            email:{
-                email: "邮箱名输入不正确"
-            },
-            psw:{
-                required: "密码不能为空",
-                minlength: "密码长度不能少于6个字符",
-                maxlength: "密码长度不能超过16个字符"
-            },
-            repsw:{
-                required: "确认密码不能为空",
-                equalTo: "确认密码和密码不一致"
-            },
-            check: {
-                required: "请先同意本站条款"
-            }
+	$('#regsubmit').on('click',function(){
+
+		$('#regForm').validate({
+			rules: {
+	            email:{
+	                required: true,
+	                email: true
+	            },
+	            psw:{
+	                required: true,
+	                minlength: 6,
+	                maxlength: 16
+	            },
+	            repsw:{
+	                required: true,
+	                equalTo: "#psw"
+	            },
+	            check: {
+	                required: true               
+	            }
+	        },
+	        messages:{
+	            email:{
+	                email: "邮箱名输入不正确"
+	            },
+	            psw:{
+	                required: "密码不能为空",
+	                minlength: "密码长度不能少于6个字符",
+	                maxlength: "密码长度不能超过16个字符"
+	            },
+	            repsw:{
+	                required: "确认密码不能为空",
+	                equalTo: "确认密码和密码不一致"
+	            },
+	            check: {
+	                required: "请先同意本站条款"
+	            }
+			}
+		});
+		alert("恭喜您注册成功！为了您的账户安全，一封邮件发送到了您注册账号的邮箱，请您抽取宝贵时间进行验证。");
+			
+	});
+	$('#psw').on('focus',function(){
+		if($(this).val().length>8){
+			$('#mid').addClass('password_on');
+			$('#low').removeClass('password_on');
 		}
 	});
+	
 });
