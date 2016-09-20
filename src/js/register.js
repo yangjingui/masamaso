4103837945a791243e617dc8aa6b46d925336bb8
yangjingui <791348547@qq.com>
2016-09-20 19:25:05 +0800

    “过滤注册界面表单验证功能实现，但是密码强度判断还没实现“

--- src/js/register.js
+++ src/js/register.js
@@ -1,16 +1,41 @@
 jQuery(function($){
 	// 表单验证插件
-	$("form").validate({
-		//设置验证规则
-		rules:{
-			psw:{required:true,rangelength:[6,16]},
-			email:{email:true,required:true},
-			url:{url:true}
-		},
-		//设置提示信息
-//		messages:{
-//			psw:{required:'用户名不能为空',minlength:'至少输入三个字符'},
-//			email:{email:'邮箱名输入不正确'}
-//		}
+	var $form = $('.login_info').find('form');
+	$('#regForm').validate({
+		rules: {
+            email:{
+                required: true,
+                email: true
+            },
+            psw:{
+                required: true,
+                minlength: 6,
+                maxlength: 16
+            },
+            repsw:{
+                required: true,
+                equalTo: "#psw"
+            },
+            check: {
+                required: true               
+            }
+        },
+        messages:{
+            email:{
+                email: "邮箱名输入不正确"
+            },
+            psw:{
+                required: "密码不能为空",
+                minlength: "密码长度不能少于6个字符",
+                maxlength: "密码长度不能超过16个字符"
+            },
+            repsw:{
+                required: "确认密码不能为空",
+                equalTo: "确认密码和密码不一致"
+            },
+            check: {
+                required: "请先同意本站条款"
+            }
+		}
 	});
 });
