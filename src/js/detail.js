680ca22ac175b1861aa79ba9d38cf00078ba8059
yangjingui <791348547@qq.com>
2016-09-20 15:44:53 +0800

    “给商品详情页的商品颜色和尺寸选择绑定点击事件”

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,6 +1,7 @@
+ //给左侧小图绑定点击事件
  jQuery(function($){
 	var $boxLi = $('#box li');
-	$('.s_tpc').on('click','li',function(){console.log(111)
+	$('.s_tpc').on('click','li',function(){
 		var idx = $(this).index();
 		$boxLi.eq(idx).show().siblings('li').hide();
 		$(this).addClass('on').siblings('li').removeClass('on');
@@ -10,5 +11,20 @@
 			width:400,
 			height:400
 		});
+	})
+});
+
+//给商品颜色选择绑定点击事件
+jQuery(function($){	
+	$('.goods_color').on('click','li',function(){	
+		$(this).addClass('color_red').siblings('li').removeClass('color_red');		
+	})
+});
+
+//给商品尺寸绑定点击事件
+jQuery(function($){	
+	$('#selectSizeBox').on('click','li',function(){
+		var idx = $(this).index();		
+		$(this).addClass('cur').siblings('li').removeClass('cur');
 	})
 });
