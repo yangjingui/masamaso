cbc626235d61a6d9e16ee37d9db361fc80c1cb96
yangjingui <791348547@qq.com>
2016-09-23 12:47:37 +0800

    优化购物车

--- src/js/detail.js
+++ src/js/detail.js
@@ -18,110 +18,90 @@
 	})
 });
 
-//给商品颜色选择绑定点击事件
-jQuery(function($){	
-	$('.goods_color').on('click','li',function(){	
-		$(this).addClass('color_red').siblings('li').removeClass('color_red');		
-	})
-});
-
-//给商品尺寸绑定点击事件
-// jQuery(function($){	
-// 	$('#selectSizeBox').on('click','li',function(){
-// 		var idx = $(this).index();		
-// 		$(this).addClass('cur').siblings('li').removeClass('cur');
-// 	})
-// });
-
-// 当点击“添加到购物车”，就添加 cookie 
-
-onload = function() {
-
-	var str = getCookie("arr");
-
-	// 用于存储所有的商品
+jQuery(function($){
+	var str = getCookie('arr');
 	var arr = [];
-	if (str != ""){
-		// 说明之前 cookie 中有商品的内容
-		// 取出来转换成数组
+	if(str != ""){
 		arr = JSON.parse(str);
 	}
-	//var arr = [];
-
-	// 商品由 名称 和 价格组成
-	// 例如：
-	// {product: XXX, price: 180}
-
-	var oBtn = document.getElementsByClassName("goods_ri_btn")[0];
+	var oBtn = $("#submit_btn2");
 	//购物车商品数
-	var oTab = document.getElementsByClassName("tab_sup_bd")[0];
-	var oAdd = document.getElementById('add_num');
+	var oTab = $(".tab");
+	var oAdd = $('#add_num');
+	var $tab = parseInt(oTab.html());
+	var $add = parseInt(oAdd.html());
+	
 	//商品款号
-	var oNo = document.getElementById("No");
+	var oNo = $("#No");
 	
 	//商品名
-	var oName = document.getElementById("name");
+	var oName = $("#name");
 	// 这个是商品价格
 	//现价
-	var NowPrice = document.getElementsByClassName("price24")[0];
+	var NowPrice = $(".price24");
 	//原价
-	var OldPrice = document.getElementById('old_price');
+	var OldPrice = $('#old_price');
 	
 	// 商品数量
-	var oNum = document.getElementById("num");
+	var oNum = $("#num");
+	var $num = parseInt(oNum.val());
 	// 这个商品的尺寸
-	var oSizeBox = document.getElementById('selectSizeBox');	
-	var oSizeLi = oSizeBox.getElementsByTagName('li');
-	var oSize = oSizeBox.getElementsByTagName('i');
+	var oSizeBox = $('#selectSizeBox');	
+	var oSizeLi = oSizeBox.find('li');
+	var oSize = oSizeLi.find('i');
 
-	var obj = {};	
-	for(var i = 0;i < oSizeLi.length;i++){
-		oSizeLi[i].xxx = i;
-		oSizeLi[i].onclick = function(){
-			var idx = this.xxx;
-			for (var k=0;k<oSizeLi.length ;k++ ){
-				oSizeLi[k].className = '';//将所有按钮的样式都去掉，即设为“”空值
-			}
-			oSizeLi[idx].className = 'cur';
-			obj.oSize = oSize[idx].innerText;
-//				alert(obj.oSize);
-		}	
-	}		
+	var obj = {};
+	//给商品颜色选择绑定点击事件
+	$('.goods_color').on('click','li',function(){
+		var idx = $(this).index();
+		$(this).addClass('color_red').siblings('li').removeClass('color_red');
+		obj.img = $(this).find('img').attr('src');
+//		console.log(obj.img)
+	});
 	
-	oBtn.onclick = function() {
+	//给商品尺寸绑定点击事件
+	oSizeLi.on('click',function(){
+		var idx = $(this).index();		
+		$(this).addClass('cur').siblings('li').removeClass('cur');
+		obj.oSize = oSize.eq(idx).html();
+//		console.log(obj.oSize);
+		arr.push(obj);
+//		console.log(arr);
+	});
+	oBtn.on('click',function(){
 		
 		//购物车数量改变
-		oTab.innerHTML++;
-		oAdd.innerHTML++;
+		oTab.html($tab ++);
+		oAdd.html($add ++);
 									
-		obj.oNo = oNo.innerHTML;
-		obj.oName = oName.innerHTML;
-		obj.NowPrice = NowPrice.innerHTML;
-		obj.OldPrice = OldPrice.innerHTML;			
-		obj.oNum = oNum.value;
-	
+		obj.oNo = oNo.html();
+		obj.oName = oName.html();
+		obj.NowPrice = NowPrice.html();
+		obj.OldPrice = OldPrice.html();			
+		obj.oNum = $num;
+		
+		var $now = parseInt(NowPrice.html().substring(1));
+		var $old = parseInt(OldPrice.html().substring(1));
+		obj.sum = ($num)*($now);//总价
+		obj.favour = $old - $now;console.log(obj.sum)
 		// 将创建好的商品添加到数组中
 		arr.push(obj);
 	
-		// 将数组的内容设置到 cookie 中呢？
 		// cookie 的名字是 arr, 内容是数组中的商品，过期时间是7天以后
 		addCookie("arr", JSON.stringify(arr), 7);
 		
 		//alert("添加成功");
-	}	
+	});	
 	//给添加按钮和减去按钮绑定点击事件
-	var low = document.getElementById('low');
-	var add = document.getElementById('add');
-	low.onclick = function(){
+	var low = $('#low');
+	var add = $('#add');
+	low.on('click',function(){
 		
-		if(oNum.value>1){
-			oNum.value --;
+		if($num>1){
+			oNum.val($num --);
 		}
-	}
-	add.onclick = function(){
-		oNum.value ++;
-	}
-		
-	
-}
-
+	});
+	add.on('click',function(){console.log(111)
+		oNum.val($num ++);
+	});
+});
